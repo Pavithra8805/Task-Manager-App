c04e763d09079a9baf43a57620342ea21f209ed1
Pavithra8805 <141915927+Pavithra8805@users.noreply.github.com>
2025-04-03 16:39:46 +0530

    Update signup.jsx

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/auth/signup", { email, password });
+      await axios.post("https://task-manager-app-v07m.onrender.com/api/auth/signup", { email, password });
 
       setSuccessMessage("✅ Signup successful! Redirecting to login...");
       setErrorMessage("");
