e6f31568cd0d26ef61ff7b7cac90b40ba989af47
Pavithra8805 <141915927+Pavithra8805@users.noreply.github.com>
2025-04-12 21:02:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,11 @@ app.use(cors());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
-
+app.get('/', (req, res) => {
+    res.json({
+        message: "Welcome to the Handcrafted World",
+        database: mongoose.connection.readyState === 1 ? 'connected' : 'disconnected'
+    })
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
